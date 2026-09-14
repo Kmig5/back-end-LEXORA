f2df93cb683137f35fce69bc55936138e897cc99
Kmig5 <kurtismiguel@outlook.fr>
2026-09-14 16:16:58 +0100

    Ajout de la fonctionnalité JWT V5

--- src/main/java/com/example/lexora/user/auth/UserAuth.java
+++ src/main/java/com/example/lexora/user/auth/UserAuth.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/auth")
+@RequestMapping("/lexora/auth")
 public class UserAuth {
 
     private final AuthenticationManager authenticationManager;
